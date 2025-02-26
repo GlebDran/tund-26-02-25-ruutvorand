fbdd2b8461b7265674087aee0ce2b441157ef120
opilane <opilane@HP-CZC2349HT0>
2025-02-26 08:56:56 +0200

    form

--- tund_26_02_25_ruutvorand.py
+++ tund_26_02_25_ruutvorand.py
@@ -8,6 +8,24 @@ def aken():
     f1.pack()
     lbl=Label(f1, text="Ruutvõrrandite lahendamine",font="Calibri 26", fg="green", bg="lightblue")
     lbl.pack(side=TOP)
-    lbl_vastus=Label(1, text="lahendamine", height=4, width=60)
+    lbl_vastus=Label(1, text="lahendamine", height=4, width=60, bg="yellow")
+    lbl_vastus.pack(side=BOTTOM)
+    lbl_a=Entry(f1, font="Calibri 26", fg="green", bg="lightblue", width=3)
+    lbl_a.pack(side=LEFT)
+    x2=Label(f1, text="x**2", font="Calibri 26", fg="green", padx=10)
+    x2.pack(side=LEFT)
 
+    lbl_b=Entry(f1, font="Calibri 26", fg="green", bg="lightblue", width=3)
+    lbl_b.pack(side=LEFT)
+    x2=Label(f1, text="x+", font="Calibri 26", fg="green", padx=10)
+    x2.pack(side=LEFT)
+
+    lbl_c=Entry(f1, font="Calibri 26", fg="green", bg="lightblue", width=3)
+    lbl_c.pack(side=LEFT)
+    y=Label(f1, text="0", font="Calibri 26", fg="green", padx=10)
+    y.pack(side=LEFT)
+
+
+
+    aken.mainloop()
 aken()
